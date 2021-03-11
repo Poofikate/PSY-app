c1fd6212f077811d66e3816cfb578abc4e37981f
Poofikate <98_vit@mail.ru>
2021-03-11 13:21:02 +0300

    обновил инит слайдеров

--- src/js/sliders/bonus-tasks.js
+++ src/js/sliders/bonus-tasks.js
@@ -1,12 +1,40 @@
 import Swiper from 'swiper/swiper-bundle.min';
 
-const $sliders = document.querySelectorAll('.bonus-tasks-slider');
-if ($sliders.length) {
-  $sliders.forEach(($slider) => {
-    const slider = new Swiper($slider, {
-      slidesPerView: 'auto',
-      spaceBetween: 14,
-      watchOverflow: true,
+class SliderController {
+  constructor(selector, options) {
+    this.$sliders = document.querySelectorAll(selector);
+    this.options = options;
+
+    this.instances = [];
+
+    this.initSliders();
+  }
+
+  initSliders() {
+    if (!this.$sliders.length) return false;
+
+    this.$sliders.forEach(($slider, i) => {
+      this.instances[i] = new Swiper($slider, this.options);
     });
-  });
+  }
+
+  destroySliders() {
+    if (this.instances.length) {
+      this.instances.forEach((instance) => instance.destroy(true, false));
+    }
+  }
+
+  updateSliders() {
+    if (this.instances.length) {
+      this.instances.forEach((instance) => instance.update());
+    }
+  }
 }
+
+const sliderController = new SliderController('.bonus-tasks-slider', {
+  slidesPerView: 'auto',
+  spaceBetween: 14,
+  watchOverflow: true,
+});
+
+window.sliderController = sliderController;
