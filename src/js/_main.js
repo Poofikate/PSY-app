f4cd718b986e966144734c5af3deaf63700e622d
Poofikate <98_vit@mail.ru>
2021-06-09 08:58:00 +0300

    правки

--- src/js/_main.js
+++ src/js/_main.js
@@ -27,3 +27,39 @@ const transitionReady = () => {
   document.body.classList.add('transition-ready');
 };
 window.addEventListener('load', transitionReady);
+window.addEventListener('load', updateDeviceHeight);
+window.addEventListener('resize', updateDeviceHeight);
+
+// window.addEventListener('load', showViewport);
+// window.addEventListener('resize', showViewport);
+
+function updateDeviceHeight() {
+  // We execute the same script as before
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+function showViewport() {
+  const id = '__VIEWPORT__';
+  let $viewport = document.getElementById(id);
+
+  if (!$viewport) {
+    $viewport = document.createElement('div');
+
+    $viewport.setAttribute('id', id);
+
+    $viewport.style.position = 'fixed';
+    $viewport.style.zIndex = '9999';
+    $viewport.style.top = '15px';
+    $viewport.style.right = '15px';
+    $viewport.style.backgroundColor = '#fff';
+    $viewport.style.borderRadius = '6px';
+    $viewport.style.padding = '10px';
+    $viewport.style.fontWeight = '500';
+    $viewport.style.boxShadow = '0 0 15px rgba(0,0,0, 0.15';
+
+    document.body.appendChild($viewport);
+  }
+
+  $viewport.innerHTML = `${window.innerWidth} / ${window.innerHeight}`;
+}
